260b728552eadde6d108ee4d99794700b5a1bb98
Stino <Linksonder@gmail.com>
2015-10-29 19:29:41 +0100

    Improve tower

--- app/controllers/towerCtrl.js
+++ app/controllers/towerCtrl.js
@@ -1,9 +1,19 @@
 module.exports = function ($scope, $timeout, $mdSidenav, $log, $rootScope) {
     
-
-	$scope.level = 0;
+	var ratio = 1.743;
 	
+	var canvasWidth = document.getElementById('canvas').offsetWidth;
+	var canvasHeight = document.getElementById('canvas').offsetHeight;
+	
+	$scope.realHeight = (canvasWidth * ratio - canvasHeight);
+	$scope.level = 0;
+
 	$scope.toLevel = function(newLevel){
-		$scope.level = newLevel;
+		$scope.level = $scope.realHeight / 100 * newLevel;
+	}
+	
+	$scope.isNear = function(value){
+		value =  $scope.realHeight / 100 * value;
+		return $scope.level < (value + 20) && $scope.level > (value - 20);
 	}
 };
